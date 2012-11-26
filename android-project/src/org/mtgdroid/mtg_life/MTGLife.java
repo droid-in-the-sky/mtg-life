fd867013859aa93a6ec8846317502008cf31e015
Francisco Sant'Anna <francisco.santanna@gmail.com>
2012-11-26 17:47:01 -0200

    (^) minor adjusts

---
+++ android-project/src/org/mtgdroid/mtg_life/MTGLife.java
@@ -0,0 +1,16 @@
+package org.mtgdroid.mtg_life;
+
+import org.libsdl.app.SDLActivity; 
+import android.os.*; 
+
+/*
+ * A sample wrapper class that just calls SDLActivity
+ */
+
+public class MTGLife extends SDLActivity { protected void onCreate(Bundle
+    savedInstanceState) { super.onCreate(savedInstanceState); }
+
+    protected void onDestroy() {
+    super.onDestroy();
+    }
+
